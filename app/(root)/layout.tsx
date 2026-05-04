b7ee3495666229a324572826e02340bf0e880d8d
Farhan Nasir <farlocked@Farhans-MacBook-Air.local>
2026-05-04 19:44:53 +0530

    landing page with toggles implemented

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
+import { cn } from "@/lib/utils"
 import {Header} from "@/modules/home/header"
 import { Metadata } from "next"
-import { Footer } from "react-day-picker"
+import { Footer } from "@/modules/home/footer"
 
 
 export const metadata: Metadata = {
@@ -18,10 +19,21 @@ export default function HomeLayout({
     return (
         <>
         <Header/>
-        {/* Header*/}
-        {/* bg effect and grid */}
-        {/* main */}
-        {/* footer */}
+        <div
+        className={cn(
+          "absolute inset-0",
+          "[background-size:40px_40px]",
+          "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+          "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
+        )}
+      />
+       <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"/>
+
+        <main className="z-20 relative w-full pt-0">
+            {
+            children
+            }
+        </main>
         <Footer/>
         </>
     )
